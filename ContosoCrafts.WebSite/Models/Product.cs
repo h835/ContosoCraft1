1bfb8e9f817d6377a52b811afed4ebcb58c92415
Parneet Kaur <200440059@student.georgianc.on.ca>
2020-06-19 01:02:07 -0400

    Importing project

---
+++ ContosoCrafts.WebSite/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ContosoCrafts.WebSite.Models
+{
+    public class Product
+    {
+        public string id { get; set; }
+        
+        public string maker { get; set; }
+        [JsonPropertyName("img")]
+        public string img { get; set; }
+        public string url { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public int[] Rating { get; set; }
+        public override string ToString() => JsonSerializer.Serialize<Product>(this);
+        
+    }
+}
